660cbfeaec2ebc3c3f72c1cdf71d4b0c670795af
Gianluca-Mantovaneli <gianlucamantovaneli@gmail.com>
2024-07-24 16:26:31 -0300

    Lendo a primeira linha e transformando em V e E em int

--- Sources/main.c
+++ Sources/main.c
@@ -1,13 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 int main(int argc, char const *argv[])
 {
     // Leitura do arquivo de entrada
 
-    // FILE *file = fopen(argv[0], "r");
+    FILE *file = fopen(argv[1], "r");
+    int V, E; // V = número de vértices, E = número de arestas
 
-    printf("arg[1] = %s\n", argv[1]);
-    printf("argv[2] = %s\n", argv[2]);
+    if (file == NULL)
+    {
+        printf("Erro ao abrir o arquivo de entrada\n");
+        return 1;
+    }
 
-    // fclose(file);
+    char line[100];
+    int contadorDeLinhas = 0;
+    char *token;
+
+    while (fgets(line, sizeof(line), file) != NULL) // Lê o arquivo linha por linha
+    {
+        contadorDeLinhas++;
+        if (contadorDeLinhas == 1) // Lê a primeira linha do arquivo onde fica V e E
+        {
+            token = strtok(line, " ");
+            V = atoi(token);
+            token = strtok(NULL, " ");
+            E = atoi(token);
+        }
+    }
+
+    fclose(file);
     return 0;
 }
